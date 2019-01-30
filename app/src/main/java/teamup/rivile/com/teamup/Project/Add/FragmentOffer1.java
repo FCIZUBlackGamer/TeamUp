c75f5341d77872eca3ba144c014989083a6af113
Abdel-Rahman <abdelrahman.ali.9400@gmail.com>
2019-01-30 18:36:53 +0200

    minor fixes at seekbar and it's views at FragmentOffer1

--- app/src/main/java/teamup/rivile/com/teamup/Project/Add/FragmentOffer1.java
+++ app/src/main/java/teamup/rivile/com/teamup/Project/Add/FragmentOffer1.java
@@ -7,6 +7,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,7 +45,6 @@ public class FragmentOffer1 extends Fragment {
     EditText moneyOutFrom, moneyOutTo, moneyInFrom, moneyInTo, conFrom, conTo;
 
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -72,12 +73,17 @@ public class FragmentOffer1 extends Fragment {
         high = view.findViewById(R.id.high);
         arrowMoney = view.findViewById(R.id.arrowMoney);
         arrowContributors = view.findViewById(R.id.arrowContributors);
+
         moneyOutFrom = view.findViewById(R.id.moneyOutFrom);
         moneyOutTo = view.findViewById(R.id.moneyOutTo);
+
         moneyInFrom = view.findViewById(R.id.moneyInFrom);
         moneyInTo = view.findViewById(R.id.moneyInTo);
+
         conFrom = view.findViewById(R.id.conFrom);
         conTo = view.findViewById(R.id.conTo);
+
+
         return view;
     }
 
@@ -85,34 +91,11 @@ public class FragmentOffer1 extends Fragment {
     public void onStart() {
         super.onStart();
 
-        moneySeekbar.setRangeValues(0, 1000004);
-        moneyRequiredSeekbar.setRangeValues(0, 100000);
-        contributorSeekbar.setRangeValues(0, 15);
+        setUpSeekBarViews(0, 100000, moneyOutFrom, moneyOutTo, moneySeekbar);
+        setUpSeekBarViews(0, 100000, moneyInFrom, moneyInTo, moneyRequiredSeekbar);
+        setUpSeekBarViews(0, 15, conFrom, conTo, contributorSeekbar);
 
-        moneySeekbar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
-            @Override
-            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Object minValue, Object maxValue) {
-                moneyOutFrom.setText(minValue.toString());
-                moneyOutTo.setText(maxValue.toString());
-            }
-        });
-
-        moneyRequiredSeekbar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
-            @Override
-            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Object minValue, Object maxValue) {
-                moneyInFrom.setText(minValue.toString());
-                moneyInTo.setText(maxValue.toString());
-            }
-        });
-
-        contributorSeekbar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
-            @Override
-            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Object minValue, Object maxValue) {
-                conFrom.setText(minValue.toString());
-                conTo.setText(maxValue.toString());
-            }
-        });
-
+        setUpProjectMoneyAvailabilityViewsVisibility();
 
         money.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -176,5 +159,175 @@ public class FragmentOffer1 extends Fragment {
                 educationLevel.setCurrentStep(4);
             }
         });
+    }
+
+    private void setUpSeekBarViews(
+            final int minVal,
+            final int maxVal,
+            final EditText fromEditText,
+            final EditText toEditText,
+            RangeSeekBar seekBar) {
+        final MinTextWatcher minTextWatcher = new MinTextWatcher(fromEditText, minVal, seekBar);
+        fromEditText.addTextChangedListener(minTextWatcher);
+
+        final MaxTextWatcher maxTextWatcher = new MaxTextWatcher(toEditText, maxVal, seekBar);
+        toEditText.addTextChangedListener(maxTextWatcher);
+
+        seekBar.setRangeValues(minVal, maxVal);
+        seekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Object minValue, Object maxValue) {
+                fromEditText.removeTextChangedListener(minTextWatcher);
+                fromEditText.setText(minValue.toString());
+                fromEditText.addTextChangedListener(minTextWatcher);
+
+                toEditText.removeTextChangedListener(maxTextWatcher);
+                toEditText.setText(maxValue.toString());
+                toEditText.addTextChangedListener(maxTextWatcher);
+            }
+        });
+
+        fromEditText.setText(String.valueOf(minVal));
+        fromEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (fromEditText.getText().toString().isEmpty())
+                    fromEditText.setText(String.valueOf(minVal));
+                if (Integer.valueOf(fromEditText.getText().toString()) > Integer.valueOf(toEditText.getText().toString()))
+                    fromEditText.setText(toEditText.getText().toString());
+            }
+        });
+
+        toEditText.setText(String.valueOf(maxVal));
+        toEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (toEditText.getText().toString().isEmpty())
+                    toEditText.setText(String.valueOf(maxVal));
+                if (Integer.valueOf(toEditText.getText().toString()) < Integer.valueOf(fromEditText.getText().toString()))
+                    toEditText.setText(fromEditText.getText().toString());
+            }
+        });
+    }
+
+    private void setUpProjectMoneyAvailabilityViewsVisibility() {
+        availGroupMoney.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.avail) {
+                    moneyRequiredSeekbar.setEnabled(true);
+                    moneyInFrom.setEnabled(true);
+                    moneyInTo.setEnabled(true);
+                } else if (checkedId == R.id.notAvail) {
+                    moneyRequiredSeekbar.setEnabled(false);
+                    moneyInFrom.setEnabled(false);
+                    moneyInTo.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    private class MinTextWatcher implements TextWatcher {
+
+        private EditText mEditText;
+        private RangeSeekBar mRangeSeekBar;
+
+        private int mMinVal;
+
+        private final String MIN_ERROR_MESSAGE;
+
+        MinTextWatcher(EditText editText, int minVal, RangeSeekBar rangeSeekBar) {
+            this.mEditText = editText;
+
+            this.mMinVal = minVal;
+            this.mRangeSeekBar = rangeSeekBar;
+
+            MIN_ERROR_MESSAGE = getString(R.string.min_val_error) + String.valueOf(mMinVal) + ".";
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String val = s.toString();
+            if (val.isEmpty()) val = String.valueOf(mMinVal);
+
+            if (Integer.valueOf(val) >= mMinVal) {
+                mRangeSeekBar.setSelectedMinValue(Integer.valueOf(val));
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String val = s.toString();
+            if (val.isEmpty()) val = String.valueOf(mMinVal);
+
+            if (Integer.valueOf(val) < mMinVal) {
+                Toast.makeText(getContext(), MIN_ERROR_MESSAGE, Toast.LENGTH_SHORT).show();
+                mRangeSeekBar.setSelectedMinValue(mMinVal);
+                changeTextToMin();
+            }
+        }
+
+        private void changeTextToMin() {
+            mEditText.removeTextChangedListener(this);
+            mEditText.setText(String.valueOf(mMinVal));
+            mEditText.addTextChangedListener(this);
+        }
+    }
+
+    private class MaxTextWatcher implements TextWatcher {
+
+        private EditText mEditText;
+        private RangeSeekBar mRangeSeekBar;
+
+        private int mMaxVal;
+
+        private final String MAX_ERROR_MESSAGE;
+
+        MaxTextWatcher(EditText editText, int maxVal, RangeSeekBar rangeSeekBar) {
+            this.mEditText = editText;
+            this.mRangeSeekBar = rangeSeekBar;
+
+            this.mMaxVal = maxVal;
+
+            MAX_ERROR_MESSAGE = getString(R.string.max_error_val) + String.valueOf(mMaxVal) + ".";
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String val = s.toString();
+            if (val.isEmpty()) val = String.valueOf(mMaxVal);
+
+            if (Integer.valueOf(val) <= mMaxVal) {
+                mRangeSeekBar.setSelectedMaxValue(Integer.valueOf(val));
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String val = s.toString();
+            if (val.isEmpty()) val = String.valueOf(mMaxVal);
+
+            if (Integer.valueOf(val) > mMaxVal) {
+                Toast.makeText(getContext(), MAX_ERROR_MESSAGE, Toast.LENGTH_SHORT).show();
+                mRangeSeekBar.setSelectedMaxValue(mMaxVal);
+                changeTextToMax();
+            }
+        }
+
+        private void changeTextToMax() {
+            mEditText.removeTextChangedListener(this);
+            mEditText.setText(String.valueOf(mMaxVal));
+            mEditText.addTextChangedListener(this);
+        }
     }
 }
