95d998c06ad9bb8a265960f19a8dbf1cae627c04
fci <momen.shahen2020@gmail.com>
2019-02-07 16:21:18 +0200

    7/2/2019 s

--- app/src/main/java/teamup/rivile/com/teamup/APIS/API.java
+++ app/src/main/java/teamup/rivile/com/teamup/APIS/API.java
@@ -4,24 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class API {
-    final public static String BASE_URL = "http://www.teamupapi.rivile.com/Offer/";
+    final public static String BASE_URL = "http://www.teamupapi.rivile.com/";
     final public static String URL_TOKEN = "7RH2'Y54.M2zt,cC";
 
     final public static String REGISTER_URL = BASE_URL + "";
     final public static String LOGIN_URL = BASE_URL + "";
-    final public static String HOME_URL = BASE_URL + "ListOffer/";
+    final public static String HOME_URL = BASE_URL + "Offer/ListOffer/";
     final public static String GENERAL_MAP_URL = BASE_URL + "";
     final public static String PROFILE_URL = BASE_URL + "";
-    final public static String ADD_OFFER_URL = BASE_URL + "Add/";
+    final public static String ADD_OFFER_URL = BASE_URL + "Offer/Add/";
     final public static String EDIT_OFFER_URL = BASE_URL + "";
     final public static String DELETE_OFFER_URL = BASE_URL + "";
-    final public static String JOIN_OFFER_URL = BASE_URL + "JoinOffer/";
+    final public static String JOIN_OFFER_URL = BASE_URL + "Offer/JoinOffer/";
+    final public static String OFFER_DETAILS_URL = BASE_URL + "Offer/GetOffer/";
     final public static String LIST_OFFERS_BY_PROJECT_URL = BASE_URL + "";
     final public static String LIST_OFFERS_BY_DEPARTMENT_URL = BASE_URL + "";
     final public static String GENERAL_SEARCH_URL = BASE_URL + "";
     final public static String FILTTER_SEARCH_URL = BASE_URL + "";
-    final public static String LOAD_DEPARTMENTS_URL = BASE_URL + "ListCat/";
-    final public static String LOAD_ListOfCapTagCat_URL = BASE_URL + "ListOfCapTagCat/";
+    final public static String LOAD_DEPARTMENTS_URL = BASE_URL + "Offer/ListCat/";
+    final public static String LOAD_ListOfCapTagCat_URL = BASE_URL + "Offer/ListOfCapTagCat/";
     final public static String LOAD_TAGS_URL = BASE_URL + "";
     final public static String LIKE_URL = BASE_URL + "";
     final public static String VIEW_PROJECT_URL = BASE_URL + "";
